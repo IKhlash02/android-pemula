bdd40048b9646ec5518b41f4fa3eb3c191f03979
IKhlash02 <muhammadikhlash5@gmail.com>
2024-03-05 22:45:22 +0700

    update ui.kt to binding

--- app/src/main/java/com/example/proyekakhirsederhana/UI.kt
+++ app/src/main/java/com/example/proyekakhirsederhana/UI.kt
@@ -2,9 +2,6 @@ package com.example.proyekakhirsederhana
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.proyekakhirsederhana.databinding.ActivityUiBinding
@@ -17,44 +14,40 @@ class UI : AppCompatActivity() {
        const val UNI_DATA = "uni_data"
     }
 
-//    private lateinit var binding: ActivityUiBinding
+    private lateinit var binding: ActivityUiBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ui)
+        binding = ActivityUiBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setActionBarTitle(title)
-
-        val namaUniv: TextView = findViewById(R.id.tv_ui)
-        val detailUniv: TextView = findViewById(R.id.tv_detail_ui)
-        val photoUniv: ImageView= findViewById(R.id.img_ui)
-        val alamatUniv : TextView = findViewById(R.id.tv_alamat)
-        val nomorUniv : TextView = findViewById(R.id.tv_nomor)
 
         val univData = intent.getParcelableExtra<University>(UNI_DATA)
 
 
         if(univData != null) {
 
-            namaUniv.text = univData.name
-            detailUniv.text = univData.detail
-            photoUniv.setImageResource(univData.photo ?: 0)
-            alamatUniv.text = univData.alamat
-            nomorUniv.text = univData.telephone
 
-            setActionBarTitle(namaUniv.text.toString())
 
-            val share : Button = findViewById(R.id.action_share)
-            share.setOnClickListener{
+            binding.tvNameUi.text = univData.name
+            binding.tvDetailUi.text = univData.detail
+            binding.imgUi.setImageResource(univData.photo)
+            binding.tvAlamat.text = univData.alamat
+            binding.tvNomor.text = univData.telephone
+
+            setActionBarTitle(binding.tvNameUi.text.toString())
+
+            binding.actionShare.setOnClickListener{
                 val intent = Intent(Intent.ACTION_SEND)
                 intent.setType("image/jpeg")
-                intent.putExtra(Intent.EXTRA_STREAM, univData.photo ?: 0)
+                intent.putExtra(Intent.EXTRA_STREAM, univData.photo)
                 intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
                 startActivity(Intent.createChooser(intent, "send"))
             }
 
-            val favorit: Button = findViewById(R.id.btn_set_favorite)
-            favorit.setOnClickListener{
-                Toast.makeText(this,  "kamu menambahkan ${namaUniv.text.toString()} ke daftar favorit", Toast.LENGTH_SHORT).show()
+
+            binding.btnSetFavorite.setOnClickListener{
+                Toast.makeText(this,  "you add ${binding.tvNameUi.text} to the favorite list", Toast.LENGTH_SHORT).show()
             }
         }
     }
